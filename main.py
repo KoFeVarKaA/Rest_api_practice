6a239b725ad2054aae8edbbf829499b659cecd11
KoFeVarKaA <155472727+KoFeVarKaA@users.noreply.github.com>
2025-02-03 20:23:04 +0300

    1 задание - переход на реальную БД

--- main.py
+++ main.py
@@ -3,61 +3,39 @@ from fastapi import FastAPI
 from pydantic import BaseModel, Field, EmailStr, ConfigDict
 import uvicorn
 
+from database import SyncORM, UsersOrm
+
 app = FastAPI()
+
+SyncORM.create_tables()
+SyncORM.insert_users()
 
 class UserSchema(BaseModel):
     first_name: str
     last_name: str
     age: int
 
-
-users_table = [
-    {
-        "first_name" : "user1", 
-        "last_name" : "last_name1", 
-        "age" : 130,
-    },
-     {
-        "first_name" : "user2", 
-        "last_name" : "last_name1", 
-        "age" : 55,
-    },
-     {
-        "first_name" : "user3", 
-        "last_name" : "last_name666", 
-        "age" : 20,
-    },
-]
-
-
 @app.get("/users", summary="Получить все пользователей из БД", tags=['Основные эндпоиты'])
-def get_all() -> list[UserSchema]:
-    return users_table
+def get_all():
+    return SyncORM.select_users_all()
 
-@app.get("/users/{first_name}", summary="Получить пользователя по имени из БД", tags=['Основные эндпоиты'])
-def get_by_name(first_name):
-    for user in users_table:
-        if user["first_name"] == first_name:
-            return user
+@app.get("/users/{user_id)}", summary="Получить пользователя по имени из БД", tags=['Основные эндпоиты'])
+def get_by_name(user_id):
+    return SyncORM.select_user_id(user_id)
     
 
 @app.post("/users", summary="Добавить нового пользователя в БД", tags=['Основные эндпоиты'])
 def create_user(new_user: UserSchema):
-    users_table.append(
-        {
-            "first_name" : new_user.first_name, 
-            "last_name" : new_user.last_name, 
-            "age" : new_user.age,
-        },
-    )
+    SyncORM.insert_user(UsersOrm(
+            first_name=new_user.first_name,
+            last_name=new_user.last_name,
+            age=new_user.age,
+    ))
     return {"message": "Пользователь успешно добавлен"}
 
-@app.delete("/users/{first_name}", summary="Удалить пользователя из БД", tags=['Основные эндпоиты'])
-def delete_user(first_name):
-    for i in range(len(users_table)):
-        if users_table[i]["first_name"] == first_name:
-            users_table.pop(i)
-            break
+@app.delete("/users/{user_id}", summary="Удалить пользователя из БД", tags=['Основные эндпоиты'])
+def delete_user(user_id):
+    SyncORM.del_one(user_id)
     return {"message": "Пользователь успешно удален"}
 
 if __name__ == "__main__":
